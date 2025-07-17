0016b3d385911a20331dd3fe117ad3ca88b7ca28
BrunoRibeiro147 <brf147@gmail.com>
2025-07-17 13:41:51 -0400

    feat: update test helpers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,7 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+
+# Mocks
+Mox.defmock(BeamBot.Adapters.Providers.ProviderMock, for: BeamBot.Ports.Provider)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Beambot.Repo, :manual)
